41f16fded3dfa4006b07e41f24d593db388f7f19
Ismael <ismaelnt@icloud.com>
2024-04-22 15:28:05 -0300

    chore: init project

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import path from 'path';
+
+// import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vitest/config';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  test: {
+    environment: 'jsdom',
+    globals: true,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    open: true,
+  },
+});
